d0632e1ee45dd2853b3e3c1deaf6fff4a346006d
yuvarajm-cloud <yuvarajm18.tech@gmail.com>
2026-05-16 13:27:17 +0530

    Update kube-master-install.sh

--- kube-master-install.sh
+++ kube-master-install.sh
@@ -45,11 +45,13 @@ systemctl enable --now crio
 systemctl enable --now kubelet
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+
 
 
 
